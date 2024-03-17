75b68abb5eb2a480736f838a0939a875877337d1
gently1223 <augustjachowski1223@gmail.com>
2024-03-17 19:49:43 +0900

    readme fix

--- src/content/dashboards/Dashboard6/PageHeader.tsx
+++ src/content/dashboards/Dashboard6/PageHeader.tsx
@@ -14,6 +14,7 @@ function PageHeader() {
           id="outlined-search"
           label="Location"
           type="search"
+          
         />
         <TextField
           id="outlined-search"
